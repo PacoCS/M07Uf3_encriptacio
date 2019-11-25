5c4b8f964777682f31f5de6fb577e6138cad4079
PacoCS <pacocayuela13@gmail.com>
2019-11-25 15:27:16 +0100

    first commit

---
+++ confirmacioVulnerable.php
@@ -0,0 +1,27 @@
+
+
+
+<?php
+  
+
+
+  //$usr = $_POST['user'];
+  //$psw = $_POST['password']
+  $usr = htmlspecialchars($_POST['user']);
+  $psw = htmlspecialchars($_POST['password']);
+  try {
+        $pdo = new PDO("mysql:host=localhost;dbname=userPass", "paco", "Admin1234");
+        $query = $pdo->prepare("SELECT * FROM users where username = '". $usr ."' and password = SHA2('".$psw."',512);" );
+        $query->execute();
+        
+        $registre = $query->fetch();
+        if(!$registre){
+          echo "Contraseña o usuario incorrecto";
+        }else{
+          echo "Bienvenido ". $usr;
+        }
+    } catch (PDOException $e) {
+      echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+      exit;
+  }
+?>
---
+++ encriptacioNoVulnerable.php
@@ -0,0 +1,31 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	
+</head>
+<body background="http://orig11.deviantart.net/20eb/f/2015/030/6/f/_minflat__dark_material_design_wallpaper__4k__by_dakoder-d8fjqzu.jpg">
+<div class="login-page">
+  <div class="form">
+    <form action="confirmacioNoVulnerable.php" method="POST" class="login-form">
+      <input type="text" name="user" placeholder="username"/>
+      <input type="password" name="password" placeholder="password"/>
+      <button><span>login</span></button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</body>
+</html>
+
+
+<!-- insert into users (username, name, password) values ('Rafa', 'rafa', sha2('admin1234',512)) -->
+<!-- insert into users (username, name, password) values ('Alba', 'alba', sha2('P@ssw0rd',512)) -->
+<!-- insert into users (username, name, password) values ('Didac', 'didac', sha2('1234567890',512)) -->
